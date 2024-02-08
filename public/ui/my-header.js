9da767a9f133db2b8f74f42692bdcd76e9855497
Gebruiker47 <t.orhan.h@hotmail.com>
2024-02-08 10:12:18 +0100

    my-header.js toegevoegd.

---
+++ public/ui/my-header.js
@@ -0,0 +1,35 @@
+import { LitElement, html } from "lit-element";
+
+export class MyHeader extends LitElement {
+  static get properties() {
+    return {
+      title: { type: String },
+    };
+  }
+
+  constructor() {
+    super();
+    this.title = "Your title";
+  }
+  static styles = css`
+    :host {
+      .header {
+        width: 100%;
+        padding: 20px;
+        margin: 10px;
+        background: var(--header-background);
+        color: var(--header-color);
+      }
+    }
+  `;
+
+  render() {
+    return html`
+      <div class="header">
+        ${this.title}
+        <slot></slot>
+      </div>
+    `;
+  }
+}
+customElements.define("my-header", MyHeader);
